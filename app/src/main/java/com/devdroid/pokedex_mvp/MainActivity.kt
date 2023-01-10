3cd7ae4b5d00fd8ac788edf14c4e6595b3eff196
jP Mesquita <jp.devdroid@gmail.com>
2023-01-10 14:29:34 -0300

    Initial Commit

---
+++ app/src/main/java/com/devdroid/pokedex_mvp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.devdroid.pokedex_mvp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
