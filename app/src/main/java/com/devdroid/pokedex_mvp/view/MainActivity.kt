c160aeafdfdef32f02edf3388dd1a208fbdd8a44
jP Mesquita <jp.devdroid@gmail.com>
2023-01-11 18:22:14 -0300

    Setup mvp structure folder, strings and colors

--- app/src/main/java/com/devdroid/pokedex_mvp/view/MainActivity.kt
+++ app/src/main/java/com/devdroid/pokedex_mvp/view/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.devdroid.pokedex_mvp
+package com.devdroid.pokedex_mvp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.devdroid.pokedex_mvp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
     }
+
 }
